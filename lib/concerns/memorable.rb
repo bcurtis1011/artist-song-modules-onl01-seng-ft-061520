81a60ed7b0f7b8f46dc9398baf97546ed6286775
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-07-18 21:53:22 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,10 +2,16 @@ module Memorable
   module ClassMethods
     
     def reset_all
-      all.clear
+      self.all.clear
     end
 
     def count
-      all.count
+      self.all.count
     end
+  end
+  
+  module InstanceMethods
+    def initialize
+    end
+  end
 end
